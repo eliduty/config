1c5117b04681fbe7f2da305ea1aaf8ac17b86467
eliduty <eliduty@qq.com>
2023-06-02 17:53:00 +0800

    feat: 增加stylelint配置

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,25 @@
+# @eliduty/stylelint-config
+
+![npm](https://img.shields.io/npm/dt/@eliduty/stylelint-config) ![npm](https://img.shields.io/npm/v/@eliduty/stylelint-config)
+
+Prettier 配置。
+
+## 安装
+
+```shell
+npm install -D @eliduty/stylelint-config
+// 或
+yarn add -D @eliduty/stylelint-config
+// 或
+pnpm install -D @eliduty/stylelint-config
+```
+
+## 使用
+
+- 在.stylelintrc.js中添加如下配置：
+
+```js
+{
+  "extends": ["@eliduty/stylelint-config"]
+}
+```
---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,91 @@
+module.exports = {
+  extends: ["stylelint-config-standard", "stylelint-config-rational-order"],
+  overrides: [
+    {
+      files: ["**/*.{html,vue}"],
+      customSyntax: "postcss-html",
+    },
+    {
+      files: ["**/*.less"],
+      customSyntax: "postcss-less",
+    },
+    {
+      files: ["**/*.scss"],
+      customSyntax: "postcss-scss",
+    },
+  ],
+  rules: {
+    "selector-class-pattern":
+      "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global", "deep"],
+      },
+    ],
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "function",
+          "if",
+          "each",
+          "include",
+          "mixin",
+          "for",
+          "extend",
+          "apply",
+          "layer",
+          "tailwind",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
+    "no-duplicate-selectors": null,
+    "no-empty-source": null,
+    "unicode-bom": "never",
+    "no-descending-specificity": null,
+    "font-family-no-missing-generic-family-keyword": null,
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-colon-space-before": "never",
+    "declaration-block-trailing-semicolon": "always",
+    "rule-empty-line-before": [
+      "always",
+      {
+        ignore: ["after-comment", "first-nested"],
+      },
+    ],
+    "property-no-unknown": [
+      true,
+      {
+        ignoreProperties: ["lines"],
+      },
+    ],
+    "media-feature-name-no-unknown": [
+      true,
+      {
+        ignoreMediaFeatureNames: "min-device-pixel-ratio",
+      },
+    ],
+    "unit-no-unknown": [
+      true,
+      {
+        ignoreUnits: ["rpx"],
+      },
+    ],
+    "function-no-unknown": [
+      true,
+      {
+        ignoreFunctions: ["theme", "v-bind"],
+      },
+    ],
+    "selector-pseudo-element-no-unknown": [
+      true,
+      {
+        ignorePseudoElements: ["v-deep"],
+      },
+    ],
+  },
+};
