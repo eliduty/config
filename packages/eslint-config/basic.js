427c12bb4b0cc133c85891876ba47406acd6897b
eliduty <eliduty@qq.com>
2023-06-08 17:06:13 +0800

    chore: release eslint-config v1.0.10

--- packages/eslint-config/basic.js
+++ packages/eslint-config/basic.js
@@ -2,10 +2,9 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true
   },
   parserOptions: {
-    // ecmaVersion: "latest",
+    ecmaVersion: 2022,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
