cf0c61811b7fa6fb26a324c99cda6f08e58f3d19
eliduty <eliduty@qq.com>
2024-01-20 08:56:07 +0800

    fix: 修改未使用的变量改为警告提示

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off'
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn'
   }
 };
