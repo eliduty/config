953930cb9ca7733d70ce005fea5604639de1929f
eliduty <eliduty@qq.com>
2024-04-03 17:10:29 +0800

    chore: ts不检测变量是否定义

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -13,6 +13,7 @@ module.exports = {
     }
   },
   rules: {
+    // 'no-undef':"off",
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-unused-vars': 'warn'
